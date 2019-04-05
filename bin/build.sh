4b6127a2e8dff6402b7df2350bf158027387fc61
fumikito <guy@hametuha.com>
2019-04-06 02:06:38 +0900

    Add release setting.

--- bin/build.sh
+++ bin/build.sh
@@ -2,8 +2,23 @@
 
 set -ex
 
+rm -rf vendor
 composer install --no-dev
 npm install
 npm start
 curl -L https://raw.githubusercontent.com/fumikito/wp-readme/master/wp-readme.php | php
-
+# Remove files
+rm -rf node_modules
+rm -rf tests
+rm -rf bin
+rm phpunit.xml.dist
+rm .distignore
+rm .gitignore
+rm .svnignore
+rm composer.lock
+rm gulpfile.js
+rm .eslintrc
+rm .travis.yml
+rm package.json
+rm package-lock.json
+rm README.md
